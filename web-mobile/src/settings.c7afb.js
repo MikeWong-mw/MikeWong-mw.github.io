7261874a0a937c7f5ef3a1dfd679a1294938984d
mw <mw@gmail.com>
2018-04-17 11:19:23 +0800

    Init comit
    files for testing raycast

---
+++ web-mobile/src/settings.c7afb.js
@@ -0,0 +1,74 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "platform",
+        "wall",
+        "collider",
+        "actor",
+        "rop"
+    ],
+    collisionMatrix: [
+        [
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+        ],
+        [
+            true,
+            false,
+            null,
+            true,
+            true
+        ],
+        [
+            true,
+            false,
+            false,
+            true,
+            true
+        ],
+        [
+            true,
+            true,
+            true,
+            true,
+            true
+        ],
+        [
+            true,
+            true,
+            true,
+            true,
+            false
+        ],
+        [
+            true,
+            false,
+            false,
+            false,
+            false,
+            false
+        ]
+    ],
+    rawAssets: {
+        assets: {}
+    },
+    assetTypes: [],
+    launchScene: "db://assets/RaycastReflectVector/PhysicsTest.fire",
+    scenes: [
+        {
+            url: "db://assets/RaycastReflectVector/PhysicsTest.fire",
+            uuid: "603LyYIPVEjLPYqg58eL0u"
+        }
+    ],
+    packedAssets: {},
+    orientation: "",
+    uuids: [],
+    "md5AssetsMap": {
+        "60/60dcbc98-20f5-448c-b3d8-aa0e7c78bd2e.json": "06379"
+    }
+};
